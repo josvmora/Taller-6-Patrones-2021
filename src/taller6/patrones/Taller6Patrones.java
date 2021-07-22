e9414d1bdb72ab157396cf3e23f3fa7ac578a004
Jose Andres Vivanco Mora <69166962+josvmora@users.noreply.github.com>
2021-07-22 11:21:22 -0500

    Add files via upload
    creando repositorio

---
+++ src/taller6/patrones/Taller6Patrones.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller6.patrones;
+
+/**
+ *
+ * @author JOSE ADRIAN GUANIN
+ */
+public class Taller6Patrones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
