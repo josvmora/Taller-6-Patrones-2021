a67eeb214f51c52ae4fe2942cd7caca5d56da4b8
Jose <josvmora@espol.edu.ec>
2021-07-22 11:24:45 -0500

    a

--- src/taller6/patrones/Taller6Patrones.java
+++ src/taller6/patrones/Taller6Patrones.java
@@ -15,7 +15,7 @@ public class Taller6Patrones {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("a");
     }
     
 }
