ddbfa6eee38ec8436509879bf141ed445695d47e
Josealb97 <jame1197@hotmail.com>
2021-07-23 11:53:04 -0500

    Patron Observer

---
+++ src/Observer/ConcreteObserver.java
@@ -0,0 +1,16 @@
+package Observer;
+
+public class ConcreteObserver implements Observer {
+
+    private Observer o;
+
+    public void anadirproducto() {
+    }
+
+    public void eliminarproducto() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void update() {
+    }
+}
---
+++ src/Observer/Sujetos.java
@@ -0,0 +1,29 @@
+package Observer;
+
+import Fachada.Proveedor;
+import Fachada.Vendedor;
+import usuarios.Comprador;
+
+public class Sujetos {
+
+    private Comprador c;
+
+    private Vendedor v;
+
+    private Proveedor p;
+
+    public void anadir() {
+    }
+
+    public void anadirproducto() {
+    }
+
+    public void eliminarproducto() {
+    }
+
+    public void getState() {
+    }
+
+    public void setState() {
+    }
+}
