497e5e39ac2aeba4ad3708c060154c47b1a9fadb
JeanVillamar <74307558+JeanVillamar@users.noreply.github.com>
2021-07-22 23:38:33 -0500

    Facade

---
+++ src/Fachada/Proveedor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fachada;
+
+/**
+ *
+ * @author jeanf
+ */
+public class Proveedor implements Canal{
+
+    @Override
+    public void cargarProduct() {}
+    
+}
